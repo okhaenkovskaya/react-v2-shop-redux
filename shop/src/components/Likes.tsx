3b4c358eb134d65639cb0d6e324cb44735ba3b29
shopifydemoadmn <shopifydemoadmn@gmail.com>
2023-06-13 19:56:04 +0400

    develop project

---
+++ shop/src/components/Likes.tsx
@@ -0,0 +1,26 @@
+import {useDispatch, useSelector} from "react-redux";
+import {addLikeAction, deleteLikeAction} from "../store/likesReducer.tsx"
+
+const Likes = () => {
+    const dispatch = useDispatch();
+    const like: number = useSelector((state: any) => state.likes.like);
+
+    const addLike = ():void => {
+        dispatch(addLikeAction(1));
+    };
+
+    const deleteLike = ():void => {
+        dispatch(deleteLikeAction(1));
+    };
+
+    return (
+        <div>
+            <h1>{like}</h1>
+            <button onClick={() => addLike()}>add Like</button>
+            <button onClick={() => deleteLike()}>delete Like</button>
+            <hr />
+        </div>
+    );
+};
+
+export default Likes;
---
+++ shop/src/components/Tasks.tsx
@@ -0,0 +1,40 @@
+import {useDispatch, useSelector} from "react-redux";
+import { v4 as uuidv4 } from 'uuid';
+import {addTaskAction, deleteTaskAction} from "../store/tasksReducer.tsx";
+
+type Task = {
+    title: string;
+    id: string;
+}
+
+const Tasks = () => {
+    const dispatch = useDispatch();
+    const tasks: Task[] = useSelector((state: any) => state.tasks.tasks);
+
+    const addTask = (title: string) => {
+        const task: Task = {
+            title,
+            id: uuidv4(),
+        }
+        dispatch(addTaskAction(task));
+    };
+
+    const deleteTask = (taskID: string) => {
+        dispatch(deleteTaskAction(taskID as string));
+    };
+
+    return (
+        <div>
+            <button onClick={() => addTask(prompt())}>add task</button>
+            {tasks.map((task: Task) => (
+                <div key={task.id}>
+                    <h2>{task.title}</h2>
+                    <button onClick={() => deleteTask(task.id)}>delete Like</button>
+                </div>
+            ))}
+            <hr />
+        </div>
+    );
+};
+
+export default Tasks;
